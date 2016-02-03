eb6c549c8422342d4883f93aead405fa409798ea
DeanHougen <hougen@ou.edu>
2016-02-03 17:31:27 -0600

    Driver class added with Hello World message.

---
+++ HelloWorld/src/HelloDriver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author deanhougen
+ *
+ */
+public class HelloDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
